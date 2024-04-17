68223786b1660e557aaa5f80c459abcf22f9fe94
Adam Petrovic <adam@petrovic.com.au>
2024-04-18 08:40:25 +1000

    remove proxy host

--- tesla_http_proxy/app/run.sh
+++ tesla_http_proxy/app/run.sh
@@ -8,7 +8,6 @@ echo "-------------------------"
 echo CLIENT_ID=$CLIENT_ID
 echo "CLIENT_SECRET=Not Shown"
 echo DOMAIN=$DOMAIN
-echo PROXY_HOST=$PROXY_HOST
 echo REGION=$REGION
 
 if [ $SERVER_MODE == 1 ]; then
@@ -16,7 +15,7 @@ if [ $SERVER_MODE == 1 ]; then
   python3 /app/keyserver.py --config-base "$CONFIG_BASE"
 elif [ $SERVER_MODE == 2 ]; then
   echo "Starting temporary Python app for authentication flow"
-  python3 /app/run.py --client-id "$CLIENT_ID" --client-secret "$CLIENT_SECRET" --domain "$DOMAIN" --region "$REGION" --proxy-host "$DOMAIN" --config-base "$CONFIG_BASE"
+  python3 /app/run.py --client-id "$CLIENT_ID" --client-secret "$CLIENT_SECRET" --domain "$DOMAIN" --region "$REGION" --config-base "$CONFIG_BASE"
 elif [ $SERVER_MODE == 3 ]; then
   echo "Starting Tesla HTTP Proxy"
   tesla-http-proxy -key-file $CONFIG_BASE/tesla/com.tesla.3p.private-key.pem -cert $CONFIG_BASE/tls/cert.pem -tls-key $CONFIG_BASE/tls/key.pem -port 8099 -host 0.0.0.0 -verbose
